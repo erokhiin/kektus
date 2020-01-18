cfd287275c00df25bc1d6f536a0f5a16b7e6ccf9
mariimo <erokhinsergg@gmail.com>
2020-01-19 02:27:40 +0300

    🌱

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+const TelegramBot = require('./src/telegram-bot.js')
+
+const app = TelegramBot({
+  db,
+  token: process.env.TELEGRAM_BOT_TOKEN,
+})
