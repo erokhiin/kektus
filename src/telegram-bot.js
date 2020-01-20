80c44c9b322ae9496a8cfcdb4adf16a95673a536
mariimo <erokhinsergg@gmail.com>
2020-01-20 08:03:28 +0300

    add create a 🌱 watering reminder

--- src/telegram-bot.js
+++ src/telegram-bot.js
@@ -6,7 +6,8 @@ const ACTIONS = Object.freeze({
 })
 
 const INPUT_STATES = Object.freeze({
-  NEW_PLANT_NAME: 'NEW_PLANT_NAME',
+  NEW_BUSH_NAME: 'NEW_BUSH_NAME',
+  NEW_BUSH_SCHEDULE: 'NEW_PLANT_SCHEDULE',
 })
 
 const MAIN_MENU = [
@@ -24,9 +25,34 @@ const MAIN_MENU = [
   ],
 ]
 
+const SCHEDULES = Object.freeze({
+  EACH_3_DAYS: 'EACH_3_DAYS',
+  EACH_WEEK: 'EACH_WEEK',
+})
+
+const SCHEDULE_TIMES = {
+  [SCHEDULES.EACH_3_DAYS]: '*/30 12 */3 * *',
+  [SCHEDULES.EACH_WEEK]: '*/30 12 */7 * *',
+}
+
+const SCHEDULER_MENU = [
+  [
+    {
+      callback_data: SCHEDULES.EACH_3_DAYS,
+      text: 'Каждые 3 дня',
+    },
+  ],
+  [
+    {
+      callback_data: SCHEDULES.EACH_WEEK,
+      text: 'Каждую неделю',
+    },
+  ],
+]
 module.exports = startTelegramBot = ({ token, db }) => {
   // Bot
   const bot = new TelegramBot(token, { polling: true })
+  const cron = require('node-cron')
 
   // Db helpers
   const findRoom = id => db.get('GROW_ROOMS').find({ id })
@@ -51,7 +77,6 @@ module.exports = startTelegramBot = ({ token, db }) => {
     const chatId = msg.chat.id
     bot.sendMessage(chatId, 'Меню:', {
       reply_markup: {
-        // keyboard: MAIN_MENU,
         inline_keyboard: MAIN_MENU,
       },
     })
@@ -66,6 +91,13 @@ module.exports = startTelegramBot = ({ token, db }) => {
     const { data } = query
     const currRoomId = query.message.chat.id
     const currRoom = getCurrRoom(currRoomId)
+    console.log(currRoomId)
+
+    const createSchedule = (bushHame, schedule) => {
+      return cron.schedule(SCHEDULE_TIMES[schedule], () => {
+        bot.sendMessage(currRoomId, '👀 Время поливать цветок: ' + bushHame)
+      })
+    }
 
     switch (data) {
       case ACTIONS.BUSHES_LIST:
@@ -73,17 +105,51 @@ module.exports = startTelegramBot = ({ token, db }) => {
           .get('BUSHES')
           .filter({ growRoomId: currRoomId })
           .value()
-        const bushesView = bushes.map(BUSH => `🌱    ${BUSH.name}`).join()
+        const bushesView = bushes.map(BUSH => `🌱    ${BUSH.name}`).join('\n')
         const bushesListMessageText = `Список цветов:\n${bushesView}`
         bot.sendMessage(currRoomId, bushesListMessageText)
         break
+
       case ACTIONS.ADD_BUSH:
         db.get('GROW_ROOMS')
           .find({ id: currRoomId })
-          .assign({ inputState: INPUT_STATES.NEW_PLANT_NAME })
+          .assign({ inputState: INPUT_STATES.NEW_BUSH_NAME })
           .write()
         bot.sendMessage(currRoomId, 'Напиши название:')
-        console.log()
+        break
+
+      case SCHEDULES.EACH_3_DAYS:
+        db.get('BUSHES')
+          .filter({ growRoomId: currRoomId })
+          .find({ name: currRoom.processingBushName })
+          .assign({ schedule: SCHEDULES.EACH_3_DAYS })
+          .write()
+
+        createSchedule(currRoom.processingBushName, SCHEDULES.EACH_3_DAYS)
+
+        db.get('GROW_ROOMS')
+          .find({ id: currRoomId })
+          .assign({ processingBushName: undefined })
+          .write()
+
+        bot.sendMessage(currRoomId, '🌳 Растение добавлено!')
+        break
+
+      case SCHEDULES.EACH_WEEK:
+        db.get('BUSHES')
+          .filter({ growRoomId: currRoomId })
+          .find({ name: currRoom.processingBushName })
+          .assign({ schedule: SCHEDULES.EACH_WEEK })
+          .write()
+
+        createSchedule(currRoom.processingBushName, SCHEDULES.EACH_WEEK)
+
+        db.get('GROW_ROOMS')
+          .find({ id: currRoomId })
+          .assign({ processingBushName: undefined })
+          .write()
+
+        bot.sendMessage(currRoomId, '🌳 Растение добавлено!')
         break
     }
   })
@@ -93,23 +159,31 @@ module.exports = startTelegramBot = ({ token, db }) => {
     const currRoomId = msg.chat.id
     const currRoom = getCurrRoom(currRoomId)
 
-    console.log('\n---\n')
-    console.log(currRoomId, currRoom, currRoom.inputState)
-
     switch (currRoom.inputState) {
-      case INPUT_STATES.NEW_PLANT_NAME:
+      case INPUT_STATES.NEW_BUSH_NAME:
         const inputBush = { name: msg.text, growRoomId: currRoomId }
         const existing = db
           .get('BUSHES')
           .find(inputBush)
           .value()
-        console.log(existing)
         if (!existing) {
           db.get('BUSHES')
             .push(inputBush)
             .write()
-          bot.sendMessage(currRoomId, '🌳 Растение добавлено!')
-          console.info(`Добавление цветка:`, inputBush)
+          bot.sendMessage(
+            currRoomId,
+            '💦 Выбери как часто его нужно поливать',
+            {
+              reply_markup: {
+                inline_keyboard: SCHEDULER_MENU,
+              },
+            },
+          )
+          db.get('GROW_ROOMS')
+            .find({ id: currRoomId })
+            .assign({ inputState: INPUT_STATES.NEW_PLANT_SCHEDULE })
+            .assign({ processingBushName: inputBush.name })
+            .write()
         } else {
           bot.sendMessage(currRoomId, '✋ Такое растение уже есть!')
         }
