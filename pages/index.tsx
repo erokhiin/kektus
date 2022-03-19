1356c1bccba0c6d1f0724feb51930a12ea796da6
erokhiin <33400447+erokhiin@users.noreply.github.com>
2022-03-19 20:54:44 +0400

    upd configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
 
 const Home: NextPage = () =><>
   <h1>Work in progress</h1>
-  <Link href="/users">Users</Link>
-  <br />
-  <Link href="/arts">Arts</Link>
 </>
 
 export default Home
